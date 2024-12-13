4ff0237db2d1c602c8dd0f43f7490f1476532252
Srinidhi945 <srinidhiporeddy@gmail.com>
2024-12-13 09:08:04 +0530

    sample2.py

--- sample2.py
+++ sample2.py
@@ -3,7 +3,7 @@ basic_sal=int(input("enter salary:"))
 if (basic_sal<10000):
     HRA=(basic_sal*67)/100
     DA=(basic_sal*73)/100
-elif (basic_sal>10000):
+elif (basic_sal<=10000):
     HRA = (basic_sal * 69) / 100
     DA = (basic_sal * 76) / 100
 elif(basic_sal>20000):
